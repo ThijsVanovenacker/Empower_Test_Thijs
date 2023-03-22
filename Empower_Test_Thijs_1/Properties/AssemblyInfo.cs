13c0cb594e43cb1f6d8a70324a194fdf0b5f5d5b
Thijs <thijs.vanovenacker@skyline.be>
2023-03-22 14:26:31 +0100

    Add project files.

---
+++ Empower_Test_Thijs_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_Test_Thijs_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_Test_Thijs_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3C57CFCE-1B29-4CCD-97A7-91D94F11D9E7")]
+[assembly: AssemblyVersion("1.0.0.0")]
