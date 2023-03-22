b6946e776ae60b5e8bf44c133f5506c1b87c4cc8
Thijs <thijs.vanovenacker@skyline.be>
2023-03-22 15:11:56 +0100

    added OrderFactory Code

--- Empower_Test_Thijs_1/Empower_Test_Thijs_1.cs
+++ Empower_Test_Thijs_1/Empower_Test_Thijs_1.cs
@@ -53,6 +53,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using Empower.Library.RoomTest;
 using Skyline.DataMiner.Automation;
 using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -77,5 +78,8 @@ public class Script
 		{
 			engine.GenerateInformation("Element Name = " + element.Name);
 		}
+
+
+		var order = OrderFactory.CreateOrder("Order 999");
 	}
 }
